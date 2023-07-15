521b03d6af410cc45fcc8384d3d5a4a0c949c248
Artur <valinurov.art@mail.ru>
2023-07-15 14:23:53 +0500

    movieDB

--- js/app.js
+++ js/app.js
@@ -1,8 +1,16 @@
-const obj = {
-   1 : '34',
-   '2': 3,
-   'fdss': 'sdf'
+const numberOfFlims = +prompt('Сколько фильмов вы уже посмотрели?','');
+
+const personalMovieDB = {
+   count: numberOfFlims,
+   movies: {},
+   actors: {},
+   genres: [],
+   privat: false
 }
-const gr = 'priv';
-obj[gr] = '23fe';
-console.log(obj);
+
+const a = prompt('Один из последних просмотренных фильмов?',''),
+      b = prompt('На сколько оцените его?');
+
+personalMovieDB.movies[a] = b;
+
+console.log(personalMovieDB);
